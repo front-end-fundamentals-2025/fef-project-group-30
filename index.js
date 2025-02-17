08b5f58f599d5b800eb518eefbc3921860882c3e
Yuhong <jiyu24ln@student.ju.se>
2025-02-17 14:33:09 +0100

    Change some visual details

--- index.js
+++ index.js
@@ -9,3 +9,17 @@ document.querySelectorAll('.dropdown').forEach(item => {
         dropdownContent.style.height = '0';
     });
 });
+
+document.addEventListener('DOMContentLoaded', function () {
+    const leftHamburger = document.getElementById('left-hamburger');
+    const rightHamburger = document.getElementById('right-hamburger');
+    const mainNav = document.querySelector('.main-nav');
+
+    leftHamburger.addEventListener('click', function () {
+        mainNav.classList.toggle('active');
+    });
+
+    rightHamburger.addEventListener('click', function () {
+        mainNav.classList.toggle('active');
+    });
+});
